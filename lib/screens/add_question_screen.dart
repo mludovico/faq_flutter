80cd535711309f0b12392a905edf3776ff3f41ea
marcelo <marcelo@hexplore.com>
2021-03-27 23:21:05 -0300

    Add validations and small refactors

--- lib/screens/add_question_screen.dart
+++ lib/screens/add_question_screen.dart
@@ -16,6 +16,7 @@ class _AddQuestionScreenState extends State<AddQuestionScreen> {
   int selected = 0;
   TextEditingController _questionController = TextEditingController();
   TextEditingController _answerController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,86 +34,92 @@ class _AddQuestionScreenState extends State<AddQuestionScreen> {
         ),
         child: ListView(
           children: [
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Título da pergunta',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        gapPadding: 5,
+            Form(
+              key: formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Título da pergunta',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          gapPadding: 5,
+                        ),
+                      ),
+                      controller: _questionController,
+                      validator: validateFields,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Resposta da pergunta',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            gapPadding: 5),
+                      ),
+                      controller: _answerController,
+                      validator: validateFields,
+                      minLines: 4,
+                      maxLines: 4,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      'Cor',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300
                       ),
                     ),
-                    controller: _questionController,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Resposta da pergunta',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5),
-                          gapPadding: 5),
-                    ),
-                    controller: _answerController,
-                    minLines: 4,
-                    maxLines: 4,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: ColorPicker(selected: selected, onTap: setCurrentColor),
                   ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Text(
-                    'Cor',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w300
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
-                  child: ColorPicker(selected: selected, onTap: setCurrentColor),
-                ),
-                ],
-              ),
+            ),
               Padding(
                 padding: const EdgeInsets.only(top: 24),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(YELLOW),
-                    shape: MaterialStateProperty.all(ContinuousRectangleBorder()),
-                    padding: MaterialStateProperty.all(
-                      EdgeInsets.symmetric(vertical: 10)
-                    )
-                  ),
-                  onPressed: () => addQA(),
-                  child: StreamBuilder<bool>(
-                    stream: _bloc.outLoading,
-                    builder: (context, snapshot) {
-                      if(snapshot.hasData && !snapshot.data) {
-                        return Text(
-                          'Adicionar',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: BLUE,
-                            fontWeight: FontWeight.bold),
-                        );
-                      } else {
-                        return SizedBox(
-                          height: 26,
-                          width: 26,
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation(BLUE),
-                          ),
-                        );
-                      }
-                    }
-                  ),
+                child: StreamBuilder<bool>(
+                  stream: _bloc.outLoading,
+                  builder: (context, snapshot) {
+                    return ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(YELLOW),
+                        shape: MaterialStateProperty.all(ContinuousRectangleBorder()),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(vertical: 10)
+                        )
+                      ),
+                      onPressed: snapshot.hasData && !snapshot.data ? addQA : null,
+                      child: snapshot.hasData && !snapshot.data
+                        ?
+                      Text(
+                        'Adicionar',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: BLUE,
+                          fontWeight: FontWeight.bold),
+                      )
+                        :
+                      SizedBox(
+                        height: 26,
+                        width: 26,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(BLUE),
+                        ),
+                      ),
+                    );
+                  }
                 ),
               )
             ],
@@ -127,7 +134,18 @@ class _AddQuestionScreenState extends State<AddQuestionScreen> {
     });
   }
 
+  String validateFields(String text) {
+    if (text == null || text.isEmpty) {
+      return 'Você deve preencher um valor válido';
+    } else {
+      return null;
+    }
+  }
+
   addQA() async {
+    bool valid = formKey.currentState.validate();
+    print(valid);
+    if (!valid) return;
     var result = await _bloc.addQuestion(
       QAPair(
         question: _questionController.text,
